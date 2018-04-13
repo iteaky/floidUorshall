b80d018149dd322b5bc21f207509b776bd5cf27a
konstantin_evstafev <o2O31991>
2018-04-14 00:38:28 +0300

    output fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
-import static java.lang.Integer.*;
+import static java.lang.Integer.MAX_VALUE;
 
 public class Main {
 
 
-    public static void routeMatrix(Integer[][] mass){
+    public static void routeMatrix(Integer[][] mass) {
         for (int i = 0; i < mass.length; i++) {
-            for (int j = 0; j < mass.length ; j++) {
+            for (int j = 0; j < mass.length; j++) {
                 int a = mass[i][j];
                 for (int k = 0; k < mass.length; k++) {
                     int b = mass[k][i];
@@ -24,20 +24,20 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Integer[][] mass = {{MAX_VALUE,MAX_VALUE,3,MAX_VALUE},
-                {2,MAX_VALUE,MAX_VALUE,MAX_VALUE},
-                {MAX_VALUE,7,MAX_VALUE,1},
-                {6,MAX_VALUE,MAX_VALUE,MAX_VALUE},};
+        Integer[][] mass = {{MAX_VALUE, MAX_VALUE, 3, MAX_VALUE},
+                {2, MAX_VALUE, MAX_VALUE, MAX_VALUE},
+                {MAX_VALUE, 7, MAX_VALUE, 1},
+                {6, MAX_VALUE, MAX_VALUE, MAX_VALUE},};
 
         routeMatrix(mass);
         System.out.println();
         for (int i = 0; i < mass.length; i++) {
             for (int j = 0; j < mass.length; j++) {
-                System.out.print(mass[i][j]+ " ");
+                System.out.print(mass[i][j] +  (mass[i][j] >= 10 ? " " : "  "));
             }
             System.out.println();
         }
 
-        }
     }
+}
 
