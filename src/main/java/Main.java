9b4c88b7a8cce0aabcc4a7fc0eb30aeb9839a8ea
konstantin_evstafev <o2O31991>
2018-04-14 23:23:13 +0300

    plus route

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,37 +3,51 @@ import static java.lang.Integer.MAX_VALUE;
 public class Main {
 
 
-    public static void routeMatrix(Integer[][] mass) {
+    public static Integer[][] routeMatrix(Integer[][] mass) {
+        Integer[][] route = new Integer[mass.length][mass.length];
         for (int i = 0; i < mass.length; i++) {
             for (int j = 0; j < mass.length; j++) {
+               if(route[i][j]==null) route[i][j] =  i + 1;
                 int a = mass[i][j];
+                if(a == MAX_VALUE) continue;
                 for (int k = 0; k < mass.length; k++) {
                     int b = mass[k][i];
                     int c = mass[k][j];
-                    if (a != MAX_VALUE && b != MAX_VALUE) {
+                    if (b != MAX_VALUE) {
                         if (c > (b + a)) {
-                            if (k != j)
+                            if (k != j) {
                                 mass[k][j] = (a + b);
-                            else mass[k][j] = 0;
+                                route[k][j] = i + 1;
+                            } else {
+                                mass[k][j] = 0;
+                            }
                         }
                     }
                 }
             }
         }
-
+        return route;
     }
 
     public static void main(String[] args) {
-        Integer[][] mass = {{MAX_VALUE, MAX_VALUE, 3, MAX_VALUE},
-                {2, MAX_VALUE, MAX_VALUE, MAX_VALUE},
-                {MAX_VALUE, 7, MAX_VALUE, 1},
-                {6, MAX_VALUE, MAX_VALUE, MAX_VALUE},};
+        Integer[][] mass = {{MAX_VALUE, 10, 30, 50, 10},
+                {MAX_VALUE, MAX_VALUE, MAX_VALUE, MAX_VALUE, MAX_VALUE},
+                {MAX_VALUE, MAX_VALUE, MAX_VALUE, MAX_VALUE, 10},
+                {MAX_VALUE, 40, 20, MAX_VALUE, MAX_VALUE},
+                {10, MAX_VALUE, 10, 30, MAX_VALUE}};
 
-        routeMatrix(mass);
+        final Integer[][] integers = routeMatrix(mass);
         System.out.println();
         for (int i = 0; i < mass.length; i++) {
             for (int j = 0; j < mass.length; j++) {
-                System.out.print(mass[i][j] +  (mass[i][j] >= 10 ? " " : "  "));
+                System.out.print(mass[i][j] + (mass[i][j] >= 10 ? " " : "  "));
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < integers.length; i++) {
+            for (int j = 0; j < integers.length; j++) {
+                System.out.print(integers[i][j] + (integers[i][j] >= 10 ? " " : "  "));
             }
             System.out.println();
         }
